8400ecaeec503006e18aa91eb2f8188a1cb8ad0e
ShrutiAngane <shrutiangane628@gmail.com>
2023-11-09 13:26:19 +0530

    changed bg color

--- Demo.js
+++ Demo.js
@@ -3,7 +3,7 @@ import { View,Text, Button } from 'react-native'
 
 const Demo = () => {
   return (
-    <View style={{backgroundColor:'green',flex:1,justifyContent:'center',alignItems:'center',gap:10}}>
+    <View style={{backgroundColor:'blue',flex:1,justifyContent:'center',alignItems:'center',gap:10}}>
         <Text style={{color:'#FFFFFF'}}>Hello! we are testing codepush</Text>
         <Text style={{color:'#FFFFFF'}}>Below button has been added on recent updates.</Text>
         <Button title='Click me' style={{backgroundColor:'blue',height:30,width:60}}></Button>
